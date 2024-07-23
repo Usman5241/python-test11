710d76a268987c7ea796e3c36bf971cb78aa6fb6
Usman5241 <161590047+Usman5241@users.noreply.github.com>
2024-07-23 05:16:52 -0700

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return ' World! my python application Running with many errors........!!! I\'m host %s' % socket.gethostname()
+    return ' World! my python aps........!!! I\'m host %s' % socket.gethostname()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
