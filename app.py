5d8a35c51e09dd7f50e583b1fcda82a60e1200af
Usman5241 <161590047+Usman5241@users.noreply.github.com>
2024-07-23 06:58:40 -0700

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return ' World! my pythons    hhasasjadjcans  jasasuanda sasahusdkjdwkdj ajdadgsdkjdhafalfhanaa s........!!! I\'m host %s' % socket.gethostname()
+    return ' World! i start my applicaton of python on code deploy with many errors....!!! I\'m host %s' % socket.gethostname()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
